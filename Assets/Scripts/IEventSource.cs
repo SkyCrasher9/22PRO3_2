449f5af720a7d125ac5e1c8429ea695ab982716e
Iñigo Martinez Rubio <56839780+InigoMartinezRubio@users.noreply.github.com>
2022-11-17 14:47:42 +0200

    Mejoras

--- Assets/Scripts/IEventSource.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public interface IEventSource
-{
-    Vector3 GetPosition();
-}
--- Assets/Scripts/IWeaponTarget.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public interface IWeaponTarget
-{
-    void OnTargetHit(TargetHitInfo _info);
-}
